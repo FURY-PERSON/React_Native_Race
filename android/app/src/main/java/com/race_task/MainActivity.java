f312866e73d4f67521d25e7779e25316624e2e5e
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2023-10-06 00:53:14 +0300

    add libs

--- android/app/src/main/java/com/race_task/MainActivity.java
+++ android/app/src/main/java/com/race_task/MainActivity.java
@@ -1,4 +1,5 @@
 package com.race_task;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "RACE_TASK";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
